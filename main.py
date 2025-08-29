b71f18230310a096d5d8e71975128d46dae9847a
0x410x <alex.harner@gmail.com>
2025-08-28 18:08:39 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0
+
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+        game_clock.tick(60)
+        dt = game_clock.tick()/1000
+
+
+
+if __name__ == "__main__":
+    main()
